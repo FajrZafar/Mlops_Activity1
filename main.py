8f3df6c496a3aad1d37207589d6b187a0f4ec19c
FajrZafar <144310375+FajrZafar@users.noreply.github.com>
2023-10-23 22:49:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-class Wallet:
-    def __init__(self):
-        self.balance = 0  # Initialize the balance as an instance variable in the constructor
+class wallet:
+    balance = 0
 
     def getBalance(self):
         return self.balance
-
-    def addBalance(self, amount):
-        self.balance += amount
-
-    def removeBalance(self, amount):
-        self.balance -= amount
-
+    
+    def addBalance(self, balance):
+        self.balance += balance
+    
+    def removeBalance(self, balance):
+        self.balance -= balance
 
 #you can put the test case here in main or make a separate file (test_main) 
 # this is a test case:
